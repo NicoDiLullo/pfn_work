dbe7f164fbe3e94a7c104ba21c3502601a4b4266
NicoDiLullo <ndilullo@thacher.org>
2025-11-05 17:14:14 -0500

    mega script changes

--- pfn_example_mega.py
+++ pfn_example_mega.py
@@ -37,8 +37,8 @@ print("pfn_example.py\tWelcome!")
 ###############################################################################
 
 # data controls, can go up to 2000000 for full dataset
-#train, val, test = 75000, 10000, 15000
-train, val, test = 1500000, 250000, 250000
+train, val, test = 75000, 10000, 15000
+#train, val, test = 1500000, 250000, 250000
 use_pids = False
 #TARGET_DTYPE = np.float32
 
@@ -59,16 +59,20 @@ mc = ModelCheckpoint('best_pfn.keras', monitor='val_acc', mode='max', verbose=1,
 print('Loading the dataset ...')
 
 # load data
-X, y = qg_jets.load(train + val + test, generator='pythia', pad=True, cache_dir='/users/ndilullo/work/pfn_work/efcache')
+X0, y0 = qg_jets.load(train + val + test, generator='pythia', pad=True, cache_dir='/users/ndilullo/work/pfn_work/efcache')
 
 print('Dataset loaded!')
 
 def load_and_run(dtype, seed):
-    X = X.astype(dtype, copy=False)
+    tf.keras.utils.set_random_seed(seed)
+    if dtype is np.float16:
+        tf.keras.mixed_precision.set_global_policy('mixed_float16')
+
+    X = X0.astype(dtype, copy=True)
     print('Datatypes switched!')
 
     # convert labels to categorical
-    Y = to_categorical(y, num_classes=2)
+    Y = to_categorical(y0, num_classes=2)
 
     print('Loaded quark and gluon jets')
 
@@ -112,7 +116,7 @@ def load_and_run(dtype, seed):
             batch_size=batch_size,
             validation_data=(X_val, Y_val),
             verbose=1,
-            callbacks=[es,mc], seed=seed)
+            callbacks=[es,mc])
 
     # get predictions on test data
     preds = pfn.predict(X_test, batch_size=1000)
@@ -131,34 +135,62 @@ def load_and_run(dtype, seed):
     mults = np.asarray([np.count_nonzero(x[:,0]) for x in X])
     mass_fp, mass_tp, threshs = roc_curve(Y[:,1], -masses)
     mult_fp, mult_tp, threshs = roc_curve(Y[:,1], -mults)
-    return auc, {(mass_fp, mass_tp), (mult_fp, mult_tp)}
+    mass_auc = roc_auc_score(Y_test[:,1], masses)
+    mult_auc = roc_auc_score(Y_test[:,1], mults)
+    #return auc, {(mass_fp, mass_tp), (mult_fp, mult_tp)}
+    return auc, mass_auc, mult_auc
 
 float64_accs = []
 float32_accs = []
 float16_accs = []
+float64_mass_accs = []
+float32_mass_accs = []
+float16_mass_accs = []
+float64_mult_accs = []
+float32_mult_accs = []
+float16_mult_accs = []
 
-float64_oi = []
+'''float64_oi = []
 float32_oi = []
-float16_oi = []
+float16_oi = []'''
 
 
 for i in range(10):
-    x, f64oi = load_and_run(np.float64, seed=i*42)
-    float64_accs.append(x)
-    float64_oi.append(f64oi)
+    a, b, c  = load_and_run(np.float64, seed=i*42)
+    #x, f64oi = load_and_run(np.float64, seed=i*42)
+    float64_accs.append(a)
+    float64_mass_accs.append(b)
+    float64_mult_accs.append(c)
+    #float64_oi.append(f64oi)
 for i in range(10):
-    x, f32oi = load_and_run(np.float32, seed=i*42)
-    float32_accs.append(x)
-    float32_oi.append(f32oi)
+    a, b, c = load_and_run(np.float32, seed=i*42)
+    #x, f32oi = load_and_run(np.float32, seed=i*42)
+    float32_accs.append(a)
+    float32_mass_accs.append(b)
+    float32_mult_accs.append(c)
+    #float32_oi.append(f32oi)
 for i in range(10):
-    x, f16oi = load_and_run(np.float16, seed=i*42)
-    float16_accs.append(x)
-    float16_oi.append(f16oi)
+    a, b, c = load_and_run(np.float16, seed=i*42)
+    #x, f16oi = load_and_run(np.float16, seed=i*42)
+    float16_accs.append(a)
+    float16_mass_accs.append(b)
+    float16_mult_accs.append(c)
+    #float16_oi.append(f16oi)
 
 print("float64 accs:", float64_accs)
 print("float32 accs:", float32_accs)
 print("float16 accs:", float16_accs)
-
+'''
 print("float64 otherinfo: " + str(float64_oi))
 print("float32 otherinfo: " + str(float32_oi))
 print("float16 otherinfo: " + str(float16_oi))
+'''
+with open('pfn_results.txt', 'w') as f:
+    f.write("Float64 AUCs: " + str(float64_accs) + "\n")
+    f.write("Float32 AUCs: " + str(float32_accs) + "\n")
+    f.write("Float16 AUCs: " + str(float16_accs) + "\n")
+    '''
+    f.write("Float64 otherinfo: " + str(float64_oi) + "\n")
+    f.write("Float32 otherinfo: " + str(float32_oi) + "\n")
+    f.write("Float16 otherinfo: " + str(float16_oi) + "\n")
+    '''
