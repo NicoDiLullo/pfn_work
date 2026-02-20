dadd1db6623a13850791467c70378cd308f2ac1d
NicoDiLullo <ndilullo@thacher.org>
2026-02-19 23:36:50 -0500

    idek

--- discriminator.py
+++ discriminator.py
@@ -13,12 +13,10 @@
 
 # EnergyFlow - Python package for high-energy particle physics.
 # Copyright (C) 2017-2020 Patrick T. Komiske III and Eric Metodiev
-# modified to experiment with smaller dtypes
 
 # standard library imports
 from __future__ import absolute_import, division, print_function
-#import os
-#os.environ['TF_DISABLE_MLIR_GPU_OPS'] = '1'  # Fallback to CPU
+
 # standard numerical library imports
 
 # Data I/O and numerical imports
@@ -54,10 +52,9 @@ print("pfn_example.py\tWelcome!")
 ###############################################################################
 
 # data controls, can go up to 2000000 for full dataset
-train, val, test = 75000, 10000, 15000
-#train, val, test = 1500000, 250000, 250000
+#train, val, test = 75000, 10000, 15000
+train, val, test = 1500000, 250000, 250000
 use_pids = False
-TARGET_DTYPE = np.float32
 
 # network architecture parameters
 Phi_sizes, F_sizes = (100, 100, 128), (100, 100, 100)
@@ -76,14 +73,10 @@ mc = ModelCheckpoint('best_pfn.keras', monitor='val_acc', mode='max', verbose=1,
 print('Loading the dataset ...')
 
 # load data
+#/oscar/data/mleblan6/energyflow
 X, y = qg_jets.load(train + val + test, generator='pythia', pad=True, cache_dir='/users/ndilullo/work/pfn_work/efcache')
 
 print('Dataset loaded!')
-
-X_f32 = X.astype(np.float32, copy=True)
-X_f64 = X.astype(np.float64, copy=True)
-#X = X.astype(TARGET_DTYPE, copy=False)
-#print('Datatypes switched!')
 
 # convert labels to categorical
 Y = to_categorical(y, num_classes=2)
@@ -92,19 +85,10 @@ print('Loaded quark and gluon jets')
 
 # preprocess by centering jets and normalizing pts
 for x in X:
-    mask = x[:, 0] > TARGET_DTYPE(0)
-    w = x[mask, 0].astype(TARGET_DTYPE, copy=False)
-    coords = x[mask, 1:3]
-    wsum = w.sum(dtype=TARGET_DTYPE)
-    if wsum > TARGET_DTYPE(0):
-        num = (coords * w[:, None]).sum(axis=0, dtype=TARGET_DTYPE)
-        yphi_avg = num / wsum
-        x[mask, 1:3] -= yphi_avg.astype(TARGET_DTYPE, copy=False)
-
-    # normalize pt in TARGET_DTYPE
-    denom = x[:, 0].sum(dtype=TARGET_DTYPE)
-    if denom > TARGET_DTYPE(0):
-        x[mask, 0] /= denom
+    mask = x[:,0] > 0
+    yphi_avg = np.average(x[mask,1:3], weights=x[mask,0], axis=0)
+    x[mask,1:3] -= yphi_avg
+    x[mask,0] /= x[:,0].sum()
 
 # handle particle id channel
 if use_pids:
@@ -117,15 +101,6 @@ print('Finished preprocessing')
 # do train/val/test split 
 (X_train, X_val, X_test,
  Y_train, Y_val, Y_test) = data_split(X, Y, val=val, test=test)
-
-X_train_f64 = X_train.astype(np.float64)
-X_val_f64 = X_val.astype(np.float64)
-X_test_f64 = X_test.astype(np.float64)
-
-X_train_f32 = X_train.astype(np.float32)
-X_val_f32 = X_val.astype(np.float32)
-X_test_f32 = X_test.astype(np.float32)
-
 
 print('Done train/val/test split')
 print('Model summary:')
@@ -142,45 +117,16 @@ pfn.fit(X_train, Y_train,
         callbacks=[es,mc])
 
 # get predictions on test data
-preds64 = pfn.predict(X_test_f64, batch_size=1000)
-#np.save('preds_fp32.npy', preds[:, 1])
+preds = pfn.predict(X_test, batch_size=1000)
 
-preds32 = pfn.predict(X_test_f32, batch_size=1000)
-#np.save('preds_fp32.npy', preds[:, 1])
-
-if preds64.shape != preds32.shape:
-    raise ValueError(f"Shape mismatch: {preds64.shape} vs {preds32.shape}")
-
-plt.figure(figsize=(5, 5))
-
-# 2D density plot
-plt.hexbin(preds64, preds32, gridsize=100, bins='log')
-plt.plot([0, 1], [0, 1], 'k--', linewidth=1)  # y = x line
-
-plt.xlabel('PFN score (float64)')
-plt.ylabel('PFN score (float32)')
-plt.title('PFN output correlation: float64 vs float32')
-cb = plt.colorbar()
-cb.set_label('log10(N jets)')
-
-plt.xlim(0, 1)
-plt.ylim(0, 1)
-
-plt.tight_layout()
-plt.savefig('pfn_corr_fp64_fp32.png')
-# plt.show()
-plt.close()
-
-
-'''
 # get ROC curve
-#pfn_fp, pfn_tp, threshs = roc_curve(Y_test[:,1], preds[:,1])
+pfn_fp, pfn_tp, threshs = roc_curve(Y_test[:,1], preds[:,1])
 
 # get area under the ROC curve
-#auc = roc_auc_score(Y_test[:,1], preds[:,1])
-#print()
-#print('PFN AUC:', auc)
-#print()
+auc = roc_auc_score(Y_test[:,1], preds[:,1])
+print()
+print('PFN AUC:', auc)
+print()
 
 # get multiplicity and mass for comparison
 masses = np.asarray([ef.ms_from_p4s(ef.p4s_from_ptyphims(x).sum(axis=0)) for x in X])
@@ -188,28 +134,23 @@ mults = np.asarray([np.count_nonzero(x[:,0]) for x in X])
 mass_fp, mass_tp, threshs = roc_curve(Y[:,1], -masses)
 mult_fp, mult_tp, threshs = roc_curve(Y[:,1], -mults)
 
-# some nicer plot settings 
-plt.rcParams['figure.figsize'] = (4,4)
-plt.rcParams['font.family'] = 'serif'
-plt.rcParams['figure.autolayout'] = True
+N = 50000
+X_f32 = X[:N].astype(np.float32)
+X_f64 = X[:N].astype(np.float64)
 
-# plot the ROC curves
-plt.plot(pfn_tp, 1-pfn_fp, '-', color='black', label='PFN')
-plt.plot(mass_tp, 1-mass_fp, '-', color='blue', label='Jet Mass')
-plt.plot(mult_tp, 1-mult_fp, '-', color='red', label='Multiplicity')
+X_both = np.concatenate([X_f32, X_f64], axis=0)
+Y_both = to_categorical(np.array([0]*N + [1]*N), num_classes=2)
 
-# axes labels
-plt.xlabel('Quark Jet Efficiency')
-plt.ylabel('Gluon Jet Rejection')
+idx = np.random.permutation(len(X_both))
+X_both, Y_both = X_both[idx], Y_both[idx]
 
-# axes limits
-plt.xlim(0, 1)
-plt.ylim(0, 1)
+(X_tr, X_vl, X_te,
+ Y_tr, Y_vl, Y_te) = data_split(X_both, Y_both, val=0.1, test=0.1)
 
-# make legend and show plot
-plt.legend(loc='lower left', frameon=False)
-#plt.show()
-plt.savefig('example_roc.pdf')
+pfn_disc = PFN(input_dim=X_both.shape[-1], Phi_sizes=(100,100,128), F_sizes=(100,100,100))
+pfn_disc.fit(X_tr, Y_tr, epochs=50, batch_size=500,
+             validation_data=(X_vl, Y_vl), verbose=1)
 
-'''
-hi I'm nico
+preds_disc = pfn_disc.predict(X_te, batch_size=1000)
+auc_disc = roc_auc_score(Y_te[:,1], preds_disc[:,1])
+print(f'Float32 vs Float64 AUC: {auc_disc:.4f}')
